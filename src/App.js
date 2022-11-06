41c03787fdd2911f62b8080e9b983cb36717de64
Kaan Colak <kaancolak3458@gmail.com>
2022-11-06 03:04:11 +0300

    update files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import Etkinliklerim from "./components/Etkinliklerim";
+import Etkinliklerim from "./pages/Etkinliklerim";
 import Son from "./pages/Son";
 import AlanGuncelleme from "./pages/AlanGuncelleme";
 import BasvuruFormu from "./components/BasvuruFormu";
