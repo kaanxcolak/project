c52208981e8af6ea105c4713e3f86dfe91b959c1
Kaan Colak <kaancolak3458@gmail.com>
2022-12-11 23:33:20 +0300

    updated

--- src/components/Guncelle.js
+++ src/components/Guncelle.js
@@ -20,7 +20,7 @@ const Guncelle = () => {
 
     const deleteContact = (id) => {
         dispatch({ type: "DELETE_CONTACT", payload: id });
-        toast.success("Event Silindi!");
+        toast.success("Etkinlik Silindi!");
     };
 
     return (
